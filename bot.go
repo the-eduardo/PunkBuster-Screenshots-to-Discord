6454510ee5c840537b1c4f86a9b2493a7ed97930
eduardo <eduardo.xbox@live.com>
2022-10-05 13:41:29 -0300

    improved code, this version is already functional!

--- bot.go
+++ bot.go
@@ -10,26 +10,34 @@ import (
 	"syscall"
 )
 
-
 var (
 	BotToken = flag.String("token", "SECRET.TOKEN_HERE", "Bot token")
+	ChannelID = "YOUR_Screenshots_ChannelID" // You should create a New Channel! The bot will spam a lot of images!
 
 )
-
 func init() { flag.Parse() }
-
-func main() {
-	contentTests := "Pruu"
+		// TODO: Create a new function that delete the local files (we don't want to resend the same files every time)
+		DisgordMain() {
 
 	sc, _ := discordgo.New("Bot " + *BotToken)
 	sc.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
-		fmt.Println("Bot is ready")
+		fmt.Println("Bot job is done, closing the session!")
+		sc.Close()
 	})
-	msg, msgerr := sc.ChannelMessageSend("YOUR_CHANNEL_ID", contentTests)
-	if msgerr != nil {
-		log.Panic(msgerr)
+
+	for x := 0; x < len(fileVerify()); x++ {
+		//fmt.Println("Files found")
+		filePath := "./downloads/" + fileVerify()[x]
+		log.Println("Sending file: ", fileVerify()[x])
+		myFile, err := os.Open(filePath)
+		if err != nil {
+			log.Fatalf("Cannot open the file: %v", err)
+		}
+		_, err = sc.ChannelFileSend(ChannelID, filePath, myFile)
+		if err != nil {
+			log.Fatalf("Cannot send the file to the Channel: %v", err)
+		}
 	}
-	fmt.Println(msg)
 
 	err := sc.Open()
 	if err != nil {
@@ -41,23 +49,24 @@ func main() {
 	signal.Notify(stop, os.Interrupt, syscall.SIGINT, os.Kill)
 	<-stop
 	log.Println("Graceful shutdown")
+	stop <- os.Interrupt
 
 }
 
-func fileVerify() []string {
+func fileVerify() []string { // Check if the file exists and return as io.Reader
 	// List all files on local path downloads/
 	files, err := os.ReadDir("./Downloads/")
 	if err != nil {
-		log.Fatal("[BOT] Error on verifying Local Files", err)
+		log.Fatalf("[BOT] Error on verifying Local Files: %v", err)
 	}
 
 	if len(files) == 0 {
-		panic("No files found")
+		log.Panic("No files found")
 	}
 
 	var LocalFiles []string
 	for _, f := range files {
-		fmt.Println(f.Name())
+		//fmt.Println(f.Name())
 		LocalFiles = append(LocalFiles, f.Name())
 	}
 
--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/jlaffaye/ftp"
 	"io"
 	"log"
 	"os"
-
-	"github.com/jlaffaye/ftp"
 )
 
 const (
-	// FILE PATHS TO DOWNLOAD:
-	//ftp://
 	server     = "YOUR_SERVER:PORT"
 	user       = "YOUR_USER"
 	pass       = "YOUR_PASS"
@@ -19,7 +16,11 @@ const (
 )
 
 func main() {
-	for xpng := 0; xpng <= 1000; xpng++ {
+
+	filename := fileList()
+	fmt.Println("Found", len(filename), "files to download!\n")
+
+	for xpng := 0; xpng < len(filename); xpng++ {
 		c, err := ftp.Dial(server)
 		if err != nil {
 			log.Fatal(err)
@@ -30,10 +31,10 @@ func main() {
 		}
 		defer c.Quit()
 
-		filename := fileList(c)
+		filename := fileList()
 		filepath := serverPath + filename[xpng]
 		localPath := "downloads/" + filename[xpng]
-		log.Println("Downloading: " + filepath)
+		log.Println("Downloading: " + filename[xpng])
 
 		// Create local file
 		file, err := os.Create(localPath)
@@ -44,7 +45,7 @@ func main() {
 		res, err := c.Retr(filepath)
 		if err != nil {
 			log.Println("Error while retrieving file from server:", err)
-			// Delete the file
+			// Delete the local file on error
 			os.Remove(localPath)
 			continue
 		}
@@ -55,18 +56,46 @@ func main() {
 			log.Println("Error while copying the file: ", err)
 			continue
 		}
-		// Delete the file TODO: Comment this line to keep the file on server side
-		/*err = c.Delete(filepath)
-		if err != nil {
-			log.Println("Error on delete: ", err)
-			continue
-		}*/
+		// Comment this line to keep the file on server side
+		c.Delete(filepath) // PS: Looks like it always throws an error, but the file is deleted anyway.
 	}
+	verifyLocalFiles()
+	defer DisgordMain()
 
 }
+func verifyLocalFiles() { // Verify the integrity of the files
+	dir, err := os.Open("downloads/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dir.Close()
+	files, err := dir.Readdir(-1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files { // Delete files with 0 bits (corrupted)
+		if file.Size() == 0 {
+			fmt.Println("Deleting file: ", file.Name())
+			err := os.Remove("downloads/" + file.Name())
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
 
-// func file list that return the list
-func fileList(c *ftp.ServerConn) []string {
+// Func return the file list from the server
+func fileList() []string {
+	c, err := ftp.Dial(server)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = c.Login(user, pass)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Quit()
+
 	ftpFileList, err := c.List(serverPath)
 	if err != nil {
 		log.Fatal("Error on List: ", err)
@@ -78,11 +107,8 @@ func fileList(c *ftp.ServerConn) []string {
 	for _, file := range ftpFileList {
 		if file.Size > 1000 && file.Name != "pbsvss.htm" {
 			fileList = append(fileList, file.Name)
-
 		}
 	}
-	fmt.Printf("\t Found %v files! \n\n", len(fileList))
 	return fileList
 }
 
-// TODO - Delete file after download completed and delete the local file on error
